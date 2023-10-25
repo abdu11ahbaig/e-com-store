e1636b48951caf68dda9d93b0a0a18959df7ba09
abdu11ahbaig <baigabdullahtariq111y@gmail.com>
2023-10-25 15:55:54 +0500

    added store functionality and feature product page and basic setup

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,27 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("249fc63c-12d6-4555-aab2-ece913b9c74a");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard
+          data={billboard}
+        />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  )
+};
+
+export default HomePage;
